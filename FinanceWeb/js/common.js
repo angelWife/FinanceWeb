dee78f838ff9e5128560e00278599f9a00275a94
clarck <zhiwei.zhou@ydcfo.com.cn>
2019-09-24 23:10:33 +0800

    优化

--- FinanceWeb/js/common.js
+++ FinanceWeb/js/common.js
@@ -1,16 +1,15 @@
 $(function(){
   $("#head-nav li").hover(function(){
     var childNavBox = $(this).find(".childNavBox");
-    
     if(childNavBox.length>0){
       $(this).addClass("active").siblings().removeClass("active");
-      $(childNavBox).fadeIn();
+      $(childNavBox).stop().fadeIn();
     }
   },function(){
     var childNavBox = $(this).find(".childNavBox");
     if(childNavBox.length>0){
       $(this).removeClass("active");
-      $(childNavBox).fadeOut();
+      $(childNavBox).stop().fadeOut();
     }
   });
 });
